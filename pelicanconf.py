fd600b3f981e675e549f64052010bf2bd71221f6
Iqbal Abdullah <iqbal@xoxzo.com>
2020-06-24 08:41:45 +0900

    Add i18n plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Iqbal Abdullah'
+AUTHOR = 'iqbalabd'
 SITENAME = 'The Fortunate | A blog by Iqbal Abdullah'
 SITEURL = ''
 
@@ -26,13 +26,34 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+            ('linkedin', 'https://www.linkedin.com/in/iqbalabd'),
+            ('github', 'https://github.com/iqbalabd'),
+            ('twitter', 'https://twitter.com/iqbalabd'),
+         )
 
 DEFAULT_PAGINATION = 3
 
+DEFAULT_DATE_FORMAT = "%Y-%m-%d"
 STATIC_PATHS = ['images', 'extra/CNAME']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, }
+
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['i18n_subsites', ]
+
+I18N_UNTRANSLATED_PAGES = 'hide'
+# mapping: language_code -> settings_overrides_dict
+I18N_SUBSITES = {
+    'ja': {
+        'SITENAME': 'ザ・フォチャネット',
+        'STATIC_PATHS': STATIC_PATHS,
+    },
+    'en': {
+        'SITENAME': SITENAME,
+        'STATIC_PATHS': STATIC_PATHS,
+    },
+}
+
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
